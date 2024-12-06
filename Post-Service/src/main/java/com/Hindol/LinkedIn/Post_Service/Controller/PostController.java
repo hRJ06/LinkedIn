8a3c2dd19f4c0aff961f4579317a7391575b59b0
hRJ06 <hindolroy.2306@gmail.com>
2024-12-06 19:25:40 +0530

    chore: Configure API gateway configuration in application.yml

--- Post-Service/src/main/java/com/Hindol/LinkedIn/Post_Service/Controller/PostController.java
+++ Post-Service/src/main/java/com/Hindol/LinkedIn/Post_Service/Controller/PostController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/core")
 public class PostController {
     private final PostService postService;
     @PostMapping
